8607d2f1b6bc6f792b04138891e828dbdd54c165
Blake Owens <blake.owens@10security.com>
2022-09-08 18:55:14 -0500

    Attempt to fix failed script execution

--- hotfix.bash
+++ hotfix.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Copy all hotfix documentation (within 'docs' and 'images')
-cp -r /docs /django-DefectDojo/docs/content/en/
-cp /images/* /django-DefectDojo/docs/static/images/
+cp -r ./docs /django-DefectDojo/docs/content/en/
+cp ./images/* /django-DefectDojo/docs/static/images/
